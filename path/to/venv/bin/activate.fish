997cae0263be87a88372270bcc159c4df28472f5
jsamy <samy.joualla@ynov.com>
2024-05-30 00:12:38 +0200

    Gpt

--- path/to/venv/bin/activate.fish
+++ path/to/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nicolas/projet-Infra-Ynov/path/to/venv"
+set -gx VIRTUAL_ENV "/home/jsam/projet-infra-Ynov/path/to/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
